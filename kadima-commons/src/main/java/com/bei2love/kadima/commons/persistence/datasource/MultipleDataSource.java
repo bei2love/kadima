bfcc04e9f92d5b6259a4e072ae1288650f5612ac
Thinker <bei2love@126.com>
2015-10-27 16:58:13 +0800

    multipleDataSource commit

--- kadima-commons/src/main/java/com/bei2love/kadima/commons/persistence/datasource/MultipleDataSource.java
+++ kadima-commons/src/main/java/com/bei2love/kadima/commons/persistence/datasource/MultipleDataSource.java
@@ -1,7 +1,19 @@
 package com.bei2love.kadima.commons.persistence.datasource;
 
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+
 /**
  * Created by thinker on 15/10/27.
  */
-public class MultipleDataSource {
+public class MultipleDataSource extends AbstractRoutingDataSource {
+    private static final ThreadLocal<String> dataSourceKey = new InheritableThreadLocal<String>();
+
+    public static void setDataSourceKey(String dataSource) {
+        dataSourceKey.set(dataSource);
+    }
+
+    @Override
+    protected Object determineCurrentLookupKey() {
+        return dataSourceKey.get();
+    }
 }
